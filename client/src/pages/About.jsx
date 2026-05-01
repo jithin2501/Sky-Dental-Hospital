f04cd9420e3411daa670eff9c49792720f2b39c9
Jithin P Joji <jithinpjoji@gmail.com>
2026-05-01 12:57:54 +0530

    updated the location design

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -14,7 +14,7 @@ const branches = [
   {
     tag: 'Branch 02',
     name: 'Sky Dental Clinic',
-    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3896.7281515780164!2d75.19984197408408!3d12.401123287864277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba4894f87f0c831%3A0x8da5031e81f241df!2sDr.%20Melvin&#39;s%20D%20Care!5e0!3m2!1sen!2sin!4v1777619261053!5m2!1sen!2sin',
+    embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3896.7281515780164!2d75.19984197408408!3d12.401123287864277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba4894f87f0c831%3A0x8da5031e81f241df!2sDr.%20Melvins%20D%20Care!5e0!3m2!1sen!2sin!4v1777619261053!5m2!1sen!2sin',
     address: 'Odayanchal-Cherupuzha Rd,Odayanchal, Kodom, Kerala 671531',
   },
 ];
@@ -316,12 +316,6 @@ function About() {
             <div className="branch-card" key={i}>
               <div className="branch-body">
                 <div className="branch-head">
-                  <div className="branch-icon">
-                    <svg viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                      <polyline points="9 22 9 12 15 12 15 22" />
-                    </svg>
-                  </div>
                   <div className="branch-meta">
                     <span className="branch-tag">{b.tag}</span>
                     <span className="branch-name">{b.name}</span>
